36dcdc2eafdf37f5445aca9a1e7e272978fba127
typhonrt <support@typhonrt.org>
2018-12-10 05:28:35 -0800

    added babelParser

--- test/src/moduleBabylon.js
+++ test/src/moduleBabylon.js
@@ -1,4 +1,4 @@
-import { assert }       from 'chai';
+                                     import { assert }       from 'chai';
 
 import parsers          from './parsers';
 import * as testconfig  from './testconfig';
